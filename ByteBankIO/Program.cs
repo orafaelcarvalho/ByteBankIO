b0520e5abf6a9531acc2d5d64e7893773326104c
orafaelcarvalho <rsouzadecarvalho@gmail.com>
2023-02-16 23:41:04 -0300

    Final da aula 1 - Trabalhando com arquivos

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,10 +1,47 @@
-﻿using ByteBankIO;
+﻿ using ByteBankIO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "D:\\Projetos\\ByteBankIO\\contas.txt";
+        var numeroDeBytesLidos = -1;
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+
+        // public override int Read(byte[] array, int offset, int count);
+        // array é o buffer
+
+
+        var buffer = new byte[1024]; // 1KB
+
+        // Devoluções:
+        // O número total de bytes lidos do buffer. Isso poderá ser menor que o número de
+        // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
+        // zero, se o final do fluxo for atingido
+        
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
 
         Console.ReadLine();
     }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+
+        //foreach (var meuByte in buffer)
+        //{
+        //    Console.Write(meuByte);
+        //    Console.Write(" ");
+        //}
+    }
+
 }
