6cfea8e8f790251cf1fc7e7c295e6205a8f7472a
orafaelcarvalho <rsouzadecarvalho@gmail.com>
2023-02-17 09:49:39 -0300

    Aula 3 - Trabalhando com arquivos - Introdução

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -23,10 +23,39 @@ partial class Program
             while (!leitor.EndOfStream) // enquanto não chega ao final do arquivo
             {
                 var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
+                var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+                var msg = $"{contaCorrente.Titular.Nome} : Conta número {contaCorrente.Numero} / Agência {contaCorrente.Agencia} / Saldo {contaCorrente.Saldo}";
+                Console.WriteLine(msg);
             }
         }
 
         Console.ReadLine();
     }
+
+    static ContaCorrente ConverterStringParaContaCorrente(string linha)
+    {
+        // 375,4644,2483.13,Jonatan Silva
+
+        var campos = linha.Split(',');
+
+        var agencia = campos[0];
+        var numero = campos[1];
+        var saldo = campos[2].Replace('.', ',');
+        var nomeTitular = campos[3];
+
+        var agenciaComInt = int.Parse(agencia);
+        var numeroComInt  = int.Parse(numero);
+        var saldoComoDouble = double.Parse(saldo);
+
+        var titular = new Cliente();
+        titular.Nome = nomeTitular;
+
+        var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
+        resultado.Depositar(saldoComoDouble);
+        resultado.Titular = titular;
+
+        return resultado;
+    }
+
 }
